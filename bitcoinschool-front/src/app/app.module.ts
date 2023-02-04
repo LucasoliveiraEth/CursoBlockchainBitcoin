bba37f47096df8a32a7bd7650cc2169cbf489604
Jader <jader.development@gmail.com>
2023-02-04 11:50:26 -0300

    Componente de menu

--- bitcoinschool-front/src/app/app.module.ts
+++ bitcoinschool-front/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './dashboard/home/home.component';
+import { ContactComponent } from './dashboard/contact/contact.component';
+import { NavMenuComponent } from './dashboard/nav-menu/nav-menu.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   // AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'contact', component: ContactComponent },
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
