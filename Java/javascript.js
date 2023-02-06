935b949562dc32c1989b8df7bd5421d728ea681f
Thiago Reis <thiago@reisdesouza.com>
2023-02-06 22:53:21 +0000

    Aula 2

--- Java/javascript.js
+++ Java/javascript.js
@@ -1,18 +1,40 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-var coin = document.getElementById('coin').value;
-
-console.log(getprice("BTC"));
-
-getprice(coin)
+function obterPrecos()
 {
-    xhr.open("GET", "https://www.mercadobitcoin.net/api/" + coin + "/ticker", false);
-
-    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
-
-    const obj = JSON.parse(xhr.responseText);
-
-    console.log(obj.ticker.last);
-
-    return obj.ticker.last;
+    document.getElementById("precoBitcoin").innerHTML = fazerRequisicao("BTC");
+    document.getElementById("precoLitecoin").innerHTML = fazerRequisicao("LTC");
+    document.getElementById("precoEthereum").innerHTML = fazerRequisicao("ETH");
+    document.getElementById("precoBitcoincash").innerHTML = fazerRequisicao("BCH");
+    document.getElementById("precoDogecoin").innerHTML = fazerRequisicao("DOGE");
+    document.getElementById("precoShiba").innerHTML = fazerRequisicao("SHIB");
 }
+
+function fazerRequisicao(coinParam){
+    
+    var coin = coinParam != null ? coinParam : document.getElementById('coin').value; 
+    //var coin;
+    //if(coinParam != null)
+    //coin = coinParam
+    //else coin = coinParam : document.getElementById('coin').value;
+
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open("GET", "https://www.mercadobitcoin.net/api/" + coin + "/ticker", false);
+
+    xhttp.send();
+
+    const obj = JSON.parse(xhttp.responseText);
+
+    var valorMoeda = coinParam == "SHIB" ? "R$ " + obj.ticker.last : formatarMoedaBRL(obj.ticker.last);
+
+    if(coinParam == null)
+      document.getElementById("PrecoOutrasCriptomoedas").innerHTML = coin == "SHIB" ? "R$ " + obj.ticker.last : valorMoeda;
+    
+    return valorMoeda;
+}
+
+function formatarMoedaBRL(valor)
+{
+    //5000 -> R$ 5.000,00
+   return Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(valor);
+}
+
