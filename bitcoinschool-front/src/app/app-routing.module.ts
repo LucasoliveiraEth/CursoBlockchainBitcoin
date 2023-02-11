b3da4e9ceb31a649334b7f8c14282fa7177f9559
Jader <jader.development@gmail.com>
2023-02-11 10:13:19 -0300

    Refatoração dos modulos e rotas.

--- bitcoinschool-front/src/app/app-routing.module.ts
+++ bitcoinschool-front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './dashboard/contact/contact.component';
+import { HomeComponent } from './dashboard/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' },
+                        { path: 'contact', component: ContactComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bitcoinschool-front/src/app/app.module.ts
+++ bitcoinschool-front/src/app/app.module.ts
@@ -1,33 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './dashboard/home/home.component';
-import { ContactComponent } from './dashboard/contact/contact.component';
-import { NavMenuComponent } from './dashboard/nav-menu/nav-menu.component';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ContactComponent,
-    NavMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-   // AppRoutingModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'contact', component: ContactComponent },
-    ])
-
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
