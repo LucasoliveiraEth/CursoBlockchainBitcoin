ff93661fe9ecf719c43a8f1e0910fea6175292bc
Jader Brandão <jader.development@gmail.com>
2023-03-02 22:13:38 -0300

    diretivas ngif e ngfor

--- bitcoinschool-front/src/app/dashboard/home/home.component.ts
+++ bitcoinschool-front/src/app/dashboard/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { PrecoBitcoin } from 'src/app/models/PrecoBitcoin';
+import { TickerList } from 'src/app/models/TickerList';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,19 @@ import { PrecoBitcoin } from 'src/app/models/PrecoBitcoin';
 export class HomeComponent implements OnInit {
   public precoBitcoin : PrecoBitcoin = new PrecoBitcoin();
   public titulo: string = "preço Bitcoin: ";
+  public listaTicker: Array<TickerList> = [
+    new TickerList(), { Nome: "Bitcoin", Valor: 15.155 },
+    new TickerList(), { Nome: "Ethereum", Valor: 8.956 },
+    new TickerList(), { Nome: "Litecoin", Valor: 450.57 },
+  ];
+
   constructor(private service: DashboardService){}
 
   ngOnInit(): void {
-    this.service.get()
+    /*this.service.get()
     .subscribe(
       response => {
         this.precoBitcoin = response
-      })
+      })*/
   }
 }
