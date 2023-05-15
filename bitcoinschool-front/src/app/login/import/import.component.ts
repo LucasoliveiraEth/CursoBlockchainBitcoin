b8537b5fc06be5aec190bd395125319f5a895236
Jader <jader.development@gmail.com>
2023-05-15 15:59:54 -0300

    Refatorando a importação de carteira.

--- bitcoinschool-front/src/app/login/import/import.component.ts
+++ bitcoinschool-front/src/app/login/import/import.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-import',
@@ -7,12 +10,17 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./import.component.scss']
 })
 export class ImportComponent {
+  returnUrl: string = "";
   submitted = false;
   checkboxConfirmacao = false;
   seedForm: FormGroup;
   registerFormGroup : FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    private walletService: WalletService) {
 
     this.seedForm = this.formBuilder.group({
       seedWords: this.formBuilder.array(Array(12).fill('').map(() => this.formBuilder.control('', Validators.required))),
@@ -23,11 +31,29 @@ export class ImportComponent {
       checkboxConfirmacao : [undefined, Validators.required]
     });
 
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
   }
 
   get registerForm() { return this.registerFormGroup.controls; }
   get seedWords(): FormArray {
     return this.seedForm.get('seedWords') as FormArray;
+  }
+
+
+  importuser()
+  {
+    this.submitted = true;
+
+     if (this.registerFormGroup.invalid || this.seedForm.invalid) {
+       return;
+     }
+
+    const seedPhraseValues: string[] = this.seedWords.controls.map(control => control.value);
+    const seedPhraseString: string = seedPhraseValues.join(' ');
+    console.log("SeedPhrase:/" + seedPhraseString + "/");
+    console.log("Passwword: " + this.registerForm['password'].value);
+
   }
 
 }
