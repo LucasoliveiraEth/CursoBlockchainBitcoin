fa81ecbc37ad5834883a5f3559c3bf926741b49e
Jader <jader.development@gmail.com>
2023-05-20 10:49:36 -0300

    Implementação da importação da carteira.

--- bitcoinschool-front/src/app/services/wallet.service.ts
+++ bitcoinschool-front/src/app/services/wallet.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CreateWalletRequest } from 'src/models/CreateWalletRequest';
+import { RestoreWalletRequest } from 'src/models/RestoreWalletRequest';
 import { Wallet } from 'src/models/Wallet';
 
 @Injectable({
@@ -17,4 +18,7 @@ export class WalletService {
     return this.http.post<Wallet>(this.apiUrl + "/create", walletRequest);
   }
 
+  restore(restoreWalletRequest: RestoreWalletRequest): Observable<Wallet> {
+    return this.http.post<Wallet>(this.apiUrl + "/restore", restoreWalletRequest);
+  }
 }
