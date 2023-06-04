cc5a7f18134faf57860245a772ee839e3f2930f4
Jader <jader.development@gmail.com>
2023-06-03 22:39:10 -0300

    Criado o formulário para adicionar novo conteúdo.

--- bitcoinschool-front/src/app/dashboard/content/add-content/add-content.component.ts
+++ bitcoinschool-front/src/app/dashboard/content/add-content/add-content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-content',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-content.component.scss']
 })
 export class AddContentComponent {
+  content : any;
+  contentFormGroup! : FormGroup;
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder,
+     private toastr: ToastrService){
+
+      this.contentFormGroup = this.formBuilder.group({
+        title : [undefined, Validators.required],
+        description : [undefined, Validators.required],
+        type : ['video', Validators.required],
+        url : [undefined, Validators.required],
+        price : [undefined, Validators.required]
+      });
+  }
+
+  get contentForm() { return this.contentFormGroup.controls; }
+
+  addContent()
+  {
+    this.toastr.success('Conteúdo incluído com sucesso!');
+  }
 }
