28eb3cf3f4f86ed6bb155912d3a2b7cf84e28784
Jader <jader.development@gmail.com>
2023-06-10 09:21:06 -0300

    Inclusão de contetúdo

--- bitcoinschool-front/src/app/dashboard/content/add-content/add-content.component.ts
+++ bitcoinschool-front/src/app/dashboard/content/add-content/add-content.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { createMask } from '@ngneat/input-mask';
+import { ContentService } from 'src/app/services/content.service';
+import { Router } from '@angular/router';
+import { ContentRequest } from 'src/models/ContentRequest';
+import { Content } from 'src/models/Content';
 
 @Component({
   selector: 'app-add-content',
   templateUrl: './add-content.component.html',
   styleUrls: ['./add-content.component.scss']
 })
-export class AddContentComponent {
+export class AddContentComponent implements OnInit {
   content : any;
+  wallet!: string | null;
+  user!: string | null;
   contentFormGroup! : FormGroup;
+  contentRequest = new ContentRequest();
+  contentResponse: any;
   submitted = false;
   currencyInputMask = createMask({
     alias: 'numeric',
@@ -21,8 +29,20 @@ export class AddContentComponent {
     placeholder: '0'
   });
 
+  ngOnInit(): void {
+    this.wallet = localStorage.getItem('wallet');
+    this.user = localStorage.getItem('user');
+
+    if(!this.wallet || !this.user)
+    {
+      this.route.navigate(['/login/user']);
+    }
+  }
+
   constructor(private formBuilder: FormBuilder,
-     private toastr: ToastrService){
+     private toastr: ToastrService,
+     private route: Router,
+     private contentService: ContentService){
 
       this.contentFormGroup = this.formBuilder.group({
         title : [undefined, Validators.required],
@@ -38,6 +58,41 @@ export class AddContentComponent {
 
   addContent()
   {
-    this.toastr.success('Conteúdo incluído com sucesso!');
+    this.submitted = true;
+
+    if (this.contentFormGroup.invalid) {
+      return;
+    }
+
+    this.contentRequest.userCode = localStorage.getItem('user') ?? "";
+    this.contentRequest.title = this.contentForm['title'].value;
+    this.contentRequest.description = this.contentForm['description'].value;
+    this.contentRequest.type = this.contentForm['type'].value;
+    this.contentRequest.url = this.contentForm['url'].value;
+    this.contentRequest.free = this.contentForm['free'].value;
+
+    if(this.contentForm['free'].value === 'pago')
+    {
+        this.contentRequest.free = false;
+        this.contentRequest.price = parseFloat(this.contentForm['price'].value.replace(/[^0-9.-]+/g,""));
+    }
+    else
+    {
+      this.contentRequest.free = true;
+      this.contentRequest.price = 0;
+    }
+
+
+    this.contentRequest.transaction = "c714a19e8dd239c3f1cb4c6beb13d270a2a8c9e5b2dc4b3fe60aa8df0c923b7e"; //this.contentForm['transaction'].value;
+
+    this.contentService.createcontent(this.contentRequest)
+        .subscribe({
+          next: (response) => {
+            this.contentResponse = response
+
+            this.toastr.success('Conteúdo incluído com sucesso!');
+          },
+          error: (error) => console.log("Ocorreu erro na requisição:" + error)
+    })
   }
 }
--- bitcoinschool-front/src/app/services/content.service.ts
+++ bitcoinschool-front/src/app/services/content.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ContentRequest } from 'src/models/ContentRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,19 @@ export class ContentService {
 
   getcontent(userCode : string): Observable<any> {
     return this.http.get<any>(this.apiUrl + "/getbyusercode?userCode=" + userCode);
+  }
+
+  createcontent(contentRequest : ContentRequest) {
+
+    console.log(contentRequest.userCode);
+    console.log(contentRequest.title);
+    console.log(contentRequest.description);
+    console.log(contentRequest.type);
+    console.log(contentRequest.url);
+    console.log(contentRequest.free);
+    console.log(contentRequest.price);
+    console.log(contentRequest.transaction);
+    return this.http.post(this.apiUrl + "/create", contentRequest);
   }
 
 }
